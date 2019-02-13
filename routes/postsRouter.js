2c7f911383031b2da606333c764a0c26343d4377
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-02-13 17:40:35 -0500

    Successful DELETE by id at posts

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -1,87 +1,114 @@
-const express = require('express');
+const express = require("express");
 
-const Posts = require('../data/helpers/postDb');
+const Posts = require("../data/helpers/postDb");
 
 const router = express.Router();
 
 // GET to /api/posts
-router.get('/', async (req, res) => {
-    try {
-        const posts = await Posts.get();
-        res.status(200).json(posts);
-    } catch {
-        res.status(500).json({ error: "There was an error retrieving the posts."});
-    }
-})
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Posts.get();
+    res.status(200).json(posts);
+  } catch {
+    res.status(500).json({ error: "There was an error retrieving the posts." });
+  }
+});
 
 // GET to /api/posts/:id
-router.get('/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const post = await Posts.getById(id);
-        if (post) {
-            res.status(200).json({post});
-        } else {
-            res.status(404).json({error: "A post with the requested ID does not exist." });
-        }
-    } catch {
-        res.status(500).json({error: "There was an error retrieving the post." });
+router.get("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const post = await Posts.getById(id);
+    if (post) {
+      res.status(200).json({ post });
+    } else {
+      res
+        .status(404)
+        .json({ error: "A post with the requested ID does not exist." });
     }
+  } catch {
+    res.status(500).json({ error: "There was an error retrieving the post." });
+  }
 });
 
 // POST to /api/posts
-router.post('/', async (req, res) => {
-    try {
-        const { text, user_id } = req.body;
-        if (!text || !user_id) {
-            res.status(400).json({error: "You must submit both text and a valid user id." });
-        } else {
-            const newPost = await Posts.insert(req.body);
-            res.status(201).json({newPost});
-        }
-    } catch {
-        res.status(500).json({ error: "There was an error submitting the new post." });
+router.post("/", async (req, res) => {
+  try {
+    const { text, user_id } = req.body;
+    if (!text || !user_id) {
+      res
+        .status(400)
+        .json({ error: "You must submit both text and a valid user id." });
+    } else {
+      const newPost = await Posts.insert(req.body);
+      res.status(201).json({ newPost });
     }
+  } catch {
+    res
+      .status(500)
+      .json({ error: "There was an error submitting the new post." });
+  }
 });
 
 // PUT to /api/posts/:id
-router.put('/:id', async (req, res) => {
-    try {
+router.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { text } = req.body;
 
-        const { id } = req.params;
-        const { text } = req.body;
-
-        if (!text) {
-            res.status(400).json({error: "You must submit both text and a valid user id."})
-        } else {
-            const post = await Posts.getById(id);
-            if (post) {
-                Posts.update(id, req.body)
-                    .then(updated => {
-                        if (updated === 1) {
-                            Posts.getById(id)
-                            .then(post => {
-                                res.status(200).json(post);
-                            })
-                        } else {
-                            res.status(500).json({error: "There was an error updating the post."})
-                        }
-                    })
-                    .catch(err => {
-                        res.status(500).json({error: "There was an error updating the post."})
-                    });
+    if (!text) {
+      res
+        .status(400)
+        .json({ error: "You must submit both text and a valid user id." });
+    } else {
+      const post = await Posts.getById(id);
+      if (post) {
+        Posts.update(id, req.body)
+          .then(updated => {
+            if (updated === 1) {
+              Posts.getById(id).then(post => {
+                res.status(200).json(post);
+              });
             } else {
-                res.status(404).json({error: "The post with the specified ID does not exist."})
+              res
+                .status(500)
+                .json({ error: "There was an error updating the post." });
             }
-        }
-    } catch {
-        res.status(500).json({error: "There was an error updating the post."})
+          })
+          .catch(err => {
+            res
+              .status(500)
+              .json({ error: "There was an error updating the post." });
+          });
+      } else {
+        res
+          .status(404)
+          .json({ error: "A post with the specified ID does not exist." });
+      }
     }
-})
+  } catch {
+    res.status(500).json({ error: "There was an error updating the post." });
+  }
+});
 
 // DELETE to /api/posts/:id
-router.delete('/:id', (req, res) => {
-    
-})
+router.delete("/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const post = await Posts.getById(id)
+            if (post) {
+                Posts.remove(id)
+                    .then(deleted => {
+                        if (deleted === 1) {
+                            res.status(204).end();
+                        }
+                    })
+            } else {
+                res.status(404).json({error: "A post with the requested ID does not exist."})
+            }
+    } catch {
+        res.status(500).json({error: "There was an error deleting the post."})
+    }
+});
 
 module.exports = router;
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,87 +1,97 @@
-const express = require('express');
+const express = require("express");
 
-const Users = require('../data/helpers/userDb');
+const Users = require("../data/helpers/userDb");
 
 const router = express.Router();
 
 // GET to /api/users
-router.get('/', async (req, res) => {
-    try {
-        const users = await Users.get();
-        res.status(200).json(users);
-    } catch {
-        res.status(500).json({ error: "There was an error retrieving the information." });
-    }
-})
+router.get("/", async (req, res) => {
+  try {
+    const users = await Users.get();
+    res.status(200).json(users);
+  } catch {
+    res
+      .status(500)
+      .json({ error: "There was an error retrieving the information." });
+  }
+});
 
 // GET to /api/users/:id
-router.get('/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const user = await Users.getById(id);
-        if (user) {
-            res.status(200).json({user});
-        } else {
-            res.status(404).json({error: "A post with the requested ID does not exist." });
-        }
-    } catch {
-        res.status(500).json({error: "There was an error retrieving the post." });
+router.get("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await Users.getById(id);
+    if (user) {
+      res.status(200).json({ user });
+    } else {
+      res
+        .status(404)
+        .json({ error: "A post with the requested ID does not exist." });
     }
-})
+  } catch {
+    res.status(500).json({ error: "There was an error retrieving the post." });
+  }
+});
 
 // POST to /api/users
-router.post('/', async (req, res) => {
-    try {
-        const { name } = req.body;
-        if (!name) {
-            res.status(400).json({error: "Please enter a name." });
-        } else {
-            const newUser = await Users.insert(req.body);
-            res.status(201).json({newUser});
-        }
-    } catch {
-        res.status(500).json({ error: "There was an error adding the new user to the database." });
+router.post("/", async (req, res) => {
+  try {
+    const { name } = req.body;
+    if (!name) {
+      res.status(400).json({ error: "Please enter a name." });
+    } else {
+      const newUser = await Users.insert(req.body);
+      res.status(201).json({ newUser });
     }
+  } catch {
+    res
+      .status(500)
+      .json({
+        error: "There was an error adding the new user to the database."
+      });
+  }
 });
 
 // PUT to /api/users/:id
-router.put('/:id', async (req, res) => {
-    try {
+router.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name } = req.body;
 
-        const { id } = req.params;
-        const { name } = req.body;
-
-        if (!name) {
-            res.status(400).json({error: "You must submit a name."})
-        } else {
-            const user = await Users.getById(id);
-            if (user) {
-                Users.update(id, req.body)
-                    .then(updated => {
-                        if (updated === 1) {
-                            Users.getById(id)
-                            .then(user => {
-                                res.status(200).json(user);
-                            })
-                        } else {
-                            res.status(500).json({error: "There was an error updating the user."})
-                        }
-                    })
-                    .catch(err => {
-                        res.status(500).json({error: "There was an error updating the user."})
-                    });
+    if (!name) {
+      res.status(400).json({ error: "You must submit a name." });
+    } else {
+      const user = await Users.getById(id);
+      if (user) {
+        Users.update(id, req.body)
+          .then(updated => {
+            if (updated === 1) {
+              Users.getById(id).then(user => {
+                res.status(200).json(user);
+              });
             } else {
-                res.status(404).json({error: "The user with the specified ID does not exist."})
+              res
+                .status(500)
+                .json({ error: "There was an error updating the user." });
             }
-        }
-    } catch {
-        res.status(500).json({error: "There was an error updating the user."})
+          })
+          .catch(err => {
+            res
+              .status(500)
+              .json({ error: "There was an error updating the user." });
+          });
+      } else {
+        res
+          .status(404)
+          .json({ error: "The user with the specified ID does not exist." });
+      }
     }
-})
+  } catch {
+    res.status(500).json({ error: "There was an error updating the user." });
+  }
+});
 
 // DELETE to /api/users/:id
-router.delete('/:id', (req, res) => {
-    
-})
+router.delete("/:id", (req, res) => {});
 
 module.exports = router;
