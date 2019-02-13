d766a5859712350698577bdbb89d845e0e74ec15
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-02-13 15:58:25 -0500

    Successful GET to users and posts

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -5,8 +5,33 @@ const Posts = require('../data/helpers/postDb');
 const router = express.Router();
 
 // GET to /api/posts
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Posts.get();
+        res.status(200).json(posts);
+    } catch {
+        res.status(500).json({ error: "There was an error retrieving the information."});
+    }
+})
+
+// GET to /api/posts/:id
+router.get('/:id', (req, res) => {
+
+})
+
 // POST to /api/posts
+router.post('/', (req, res) => {
+    
+})
+
 // PUT to /api/posts/:id
+router.put('/:id', (req, res) => {
+    
+})
+
 // DELETE to /api/posts/:id
+router.delete('/:id', (req, res) => {
+    
+})
 
 module.exports = router;
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -5,8 +5,33 @@ const Users = require('../data/helpers/userDb');
 const router = express.Router();
 
 // GET to /api/users
+router.get('/', async (req, res) => {
+    try {
+        const users = await Users.get();
+        res.status(200).json(users);
+    } catch {
+        res.status(500).json({ error: "There was an error retrieving the information." });
+    }
+})
+
+// GET to /api/users/:id
+router.get('/:id', (req, res) => {
+
+})
+
 // POST to /api/users
+router.post('/', (req, res) => {
+    
+})
+
 // PUT to /api/users/:id
+router.put('/:id', (req, res) => {
+    
+})
+
 // DELETE to /api/users/:id
+router.delete('/:id', (req, res) => {
+    
+})
 
 module.exports = router;
