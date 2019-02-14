bc917204bd48766ddc99de5f674d81241bb0c4b4
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-02-13 19:04:43 -0500

    Add helmet and implement error middleware

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require("express");
+const helmet = require("helmet");
 const server = express();
 
 const usersRouter = require("./routes/usersRouter");
 const postsRouter = require("./routes/postsRouter");
 
+function errorHandler(err,req,res,next) {
+    res.status(400).json({message: "Error", err});
+}
+
 server.use(express.json());
+server.use(helmet());
 
 server.use("/api/posts", postsRouter);
 server.use("/api/users", usersRouter);
+
+server.use(errorHandler);
 
 module.exports = server;
